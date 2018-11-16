f32cdea0a58ecdce67525bdeb6f422d3bb1a3467
Isabelle Ichikawa Yagi <isabelle@FORLOGIC.LOCAL>
2018-11-16 16:09:02 -0200

    mudança de cor quando botão selecionado

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   isSidenavActive = true;
+  isClientesActive = false;
+  isAvaliacoesActive = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  clientesActive(item: String) {
+    if (item === 'Clientes') {
+      if (this.isClientesActive === true) {
+        this.isClientesActive = true;
+        this.isAvaliacoesActive = false;
+      } else {
+        this.isClientesActive = !this.isClientesActive;
+        this.isAvaliacoesActive = !this.isClientesActive;
+      }
+    }
+    console.log('Cliente ' + this.isClientesActive);
+    console.log('Avaliações ' + this.isAvaliacoesActive);
+  }
+
+  avaliacoesActive(item: String) {
+    if (item === 'Avaliações') {
+      if (this.isAvaliacoesActive === true) {
+        this.isClientesActive = false;
+        this.isAvaliacoesActive = true;
+      } else {
+        this.isAvaliacoesActive = !this.isAvaliacoesActive;
+        this.isClientesActive = !this.isAvaliacoesActive;
+      }
+    }
+    console.log('Cliente ' + this.isClientesActive);
+    console.log('Avaliações ' + this.isAvaliacoesActive);
   }
 
 }
