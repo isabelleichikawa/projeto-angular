7e96161e38153ee7ff4a871842b4caa6bd42ae2f
Isabelle Ichikawa Yagi <isabelle@FORLOGIC.LOCAL>
2018-11-19 09:58:29 -0200

    adicionando rotas para os novos componentes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,12 +4,16 @@ import { HomeComponent } from './home/home.component';
 import { ModuleWithProviders } from '@angular/compiler/src/core';
 import { ClientesComponent } from './clientes/clientes.component';
 import { AvaliacoesComponent } from './avaliacoes/avaliacoes.component';
+import { NovoClienteComponent } from './novo-cliente/novo-cliente.component';
+import { NovaAvaliacaoComponent } from './nova-avaliacao/nova-avaliacao.component';
 
 const APP_ROUTES: Routes = [
     {
         path: 'home', component: HomeComponent, children: [
-            { path: 'clientes', component: ClientesComponent },
-            { path: 'avaliacoes', component: AvaliacoesComponent }]
+            { path: 'clientes', component: ClientesComponent, children: [{ path: 'novo-cliente', component: NovoClienteComponent }] },
+            { path: 'avaliacoes', component: AvaliacoesComponent, children: [{ path: 'nova-avaliacao', component: NovaAvaliacaoComponent }]
+        }
+        ]
     }
 ];
 
